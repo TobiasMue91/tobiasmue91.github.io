a6e84093aba230a40ac7462185af8284ec2c8ec1
tobias-mueller-dh <tobias.mueller@deinhandy.de>
2023-02-24 01:53:17 +0100

    - dunno

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const CACHE = "pwabuilder-page";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
