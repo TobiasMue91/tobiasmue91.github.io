0c188e4d35362fbfa416f6bdc6e70261f4414baf
TobiasMue91 <tobias.mue91@googlemail.com>
2023-04-01 03:12:24 +0200

    - first attempt at adding a new level to the game

--- games/time_travel_agency.js
+++ games/time_travel_agency.js
@@ -2,6 +2,7 @@
 let timeCrystals = 0;
 let crystalsPerSecond = 0;
 let lastUpdate = Date.now();
+let timeEssence = 0;
 
 let upgrades = [
     {
@@ -96,84 +97,6 @@ let upgrades = [
         }
     },
     {
-        id: 'timeTraveler3',
-        name: 'Recruit Leonardo da Vinci',
-        baseCost: 100000,
-        costMultiplier: 1.6,
-        cps: 800,
-        owned: 0,
-        isVisible: false,
-        requirements: {
-            timeCrystals: 120000,
-            otherUpgrades: ['timeBattery2']
-        }
-    },
-    {
-        id: 'timeCapsule1',
-        name: 'Bronze Time Capsule',
-        baseCost: 250000,
-        costMultiplier: 1.7,
-        cps: 1500,
-        owned: 0,
-        isVisible: false,
-        requirements: {
-            timeCrystals: 300000,
-            otherUpgrades: ['timeTraveler3']
-        }
-    },
-    {
-        id: 'timeCapsule2',
-        name: 'Diamond Time Capsule',
-        baseCost: 1000000,
-        costMultiplier: 2,
-        cps: 5000,
-        owned: 0,
-        isVisible: false,
-        requirements: {
-            timeCrystals: 2000000,
-            otherUpgrades: ['timeCapsule1']
-        }
-    },
-    {
-        id: 'tardis',
-        name: 'TARDIS',
-        baseCost: 5000000,
-        costMultiplier: 2.5,
-        cps: 10000,
-        owned: 0,
-        isVisible: false,
-        requirements: {
-            timeCrystals: 10000000,
-            otherUpgrades: ['timeCapsule2']
-        }
-    },
-    {
-        id: 'timeCompressor1',
-        name: 'Time Compressor Mk I',
-        baseCost: 2000000,
-        costMultiplier: 1.8,
-        cps: 6000,
-        owned: 0,
-        isVisible: false,
-        requirements: {
-            timeCrystals: 4000000,
-            otherUpgrades: ['timeCapsule2']
-        }
-    },
-    {
-        id: 'timeCompressor2',
-        name: 'Time Compressor Mk II',
-        baseCost: 10000000,
-        costMultiplier: 2,
-        cps: 20000,
-        owned: 0,
-        isVisible: false,
-        requirements: {
-            timeCrystals: 20000000,
-            otherUpgrades: ['timeCompressor1']
-        }
-    },
-    {
         id: 'timeGoggles',
         name: 'Time Goggles',
         baseCost: 75000,
@@ -187,6 +110,19 @@ let upgrades = [
         }
     },
     {
+        id: 'timeTraveler3',
+        name: 'Recruit Leonardo da Vinci',
+        baseCost: 100000,
+        costMultiplier: 1.6,
+        cps: 800,
+        owned: 0,
+        isVisible: false,
+        requirements: {
+            timeCrystals: 120000,
+            otherUpgrades: ['timeBattery2']
+        }
+    },
+    {
         id: 'timeGloves',
         name: 'Time Gloves',
         baseCost: 150000,
@@ -197,6 +133,19 @@ let upgrades = [
         requirements: {
             timeCrystals: 200000,
             otherUpgrades: ['timeGoggles']
+        }
+    },
+    {
+        id: 'timeCapsule1',
+        name: 'Bronze Time Capsule',
+        baseCost: 250000,
+        costMultiplier: 1.7,
+        cps: 1500,
+        owned: 0,
+        isVisible: false,
+        requirements: {
+            timeCrystals: 300000,
+            otherUpgrades: ['timeTraveler3']
         }
     },
     {
@@ -223,6 +172,19 @@ let upgrades = [
         requirements: { timeCrystals: 800000, otherUpgrades: ['timeBoots'] }
     },
     {
+        id: 'timeCapsule2',
+        name: 'Diamond Time Capsule',
+        baseCost: 1000000,
+        costMultiplier: 2,
+        cps: 5000,
+        owned: 0,
+        isVisible: false,
+        requirements: {
+            timeCrystals: 2000000,
+            otherUpgrades: ['timeCapsule1']
+        }
+    },
+    {
         id: 'timeSuit2',
         name: 'Astral Armor',
         baseCost: 1200000,
@@ -233,6 +195,19 @@ let upgrades = [
         requirements: { timeCrystals: 1600000, otherUpgrades: ['timeSuit1'] }
     },
     {
+        id: 'timeCompressor1',
+        name: 'Time Compressor Mk I',
+        baseCost: 2000000,
+        costMultiplier: 1.8,
+        cps: 6000,
+        owned: 0,
+        isVisible: false,
+        requirements: {
+            timeCrystals: 4000000,
+            otherUpgrades: ['timeCapsule2']
+        }
+    },
+    {
         id: 'timeHelmet1',
         name: 'Chrono-Cap',
         baseCost: 2000000,
@@ -241,6 +216,19 @@ let upgrades = [
         owned: 0,
         isVisible: false,
         requirements: { timeCrystals: 2500000, otherUpgrades: ['timeSuit2'] }
+    },
+    {
+        id: 'tardis',
+        name: 'TARDIS',
+        baseCost: 5000000,
+        costMultiplier: 2.5,
+        cps: 10000,
+        owned: 0,
+        isVisible: false,
+        requirements: {
+            timeCrystals: 10000000,
+            otherUpgrades: ['timeCapsule2']
+        }
     },
     {
         id: 'timeHelmet2',
@@ -261,7 +249,47 @@ let upgrades = [
         owned: 0,
         isVisible: false,
         requirements: { timeCrystals: 10000000, otherUpgrades: ['timeHelmet2'] }
-    }
+    },
+    {
+        id: 'timeCompressor2',
+        name: 'Time Compressor Mk II',
+        baseCost: 10000000,
+        costMultiplier: 2,
+        cps: 20000,
+        owned: 0,
+        isVisible: false,
+        requirements: {
+            timeCrystals: 20000000,
+            otherUpgrades: ['timeCompressor1']
+        }
+    },
+];
+
+let prestigeUpgrades = [
+    {
+        id: "timeCrystalMultiplier",
+        name: "Time Crystal Multiplier",
+        baseCost: 10,
+        costMultiplier: 1.5,
+        bonusMultiplier: 0.1,
+        owned: 0,
+    },
+    {
+        id: "timeTravelerEfficiency",
+        name: "Time Traveler Efficiency",
+        baseCost: 20,
+        costMultiplier: 1.6,
+        bonusMultiplier: 0.05,
+        owned: 0,
+    },
+    {
+        id: "timeMachineUpgradeDiscount",
+        name: "Time Machine Upgrade Discount",
+        baseCost: 30,
+        costMultiplier: 1.7,
+        bonusMultiplier: 0.01,
+        owned: 0,
+    },
 ];
 
 let achievements = [
@@ -337,6 +365,7 @@ let achievements = [
     }
 ];
 
+
 /* Define game functions*/
 
 function gatherCrystals() {
@@ -351,6 +380,12 @@ function purchaseUpgrade(upgradeId) {
         timeCrystals -= upgradeCost(upgrade);
         upgrade.owned += 1;
         crystalsPerSecond += upgrade.cps;
+
+        const timeCrystalMultiplierUpgrade = prestigeUpgrades.find(p => p.id === "timeCrystalMultiplier");
+        if (timeCrystalMultiplierUpgrade) {
+            crystalsPerSecond *= (1 + timeCrystalMultiplierUpgrade.bonusMultiplier * timeCrystalMultiplierUpgrade.owned);
+        }
+
         updateDisplay();
     }
 }
@@ -388,6 +423,40 @@ function updateDisplay() {
     document.getElementById('crystals-per-second').innerText = numberWithCommas(crystalsPerSecond.toFixed(1));
 
     updateUpgradeDisplay();
+
+    if (timeEssence > 0) {
+        document.getElementById("time-essence").innerText = numberWithCommas(timeEssence.toFixed(0));
+        updatePrestigeUpgradeDisplay();
+    }
+
+    updatePrestigeVisibility();
+}
+
+function updatePrestigeVisibility() {
+    const milestoneReached = timeCrystals >= 1e6 || timeEssence > 0;
+    document.getElementById("prestige-button").style.display = milestoneReached ? "block" : "none";
+    document.getElementById("time-essence-display").style.display = milestoneReached ? "block" : "none";
+    document.getElementById("prestige-upgrades").style.display = milestoneReached ? "block" : "none";
+}
+
+function updatePrestigeUpgradeDisplay() {
+    let prestigeUpgradeListHTML = "";
+    for (const prestigeUpgrade of prestigeUpgrades) {
+        const buttonDisabled = canBuyPrestigeUpgrade(prestigeUpgrade) ? "" : ' disabled="disabled"';
+        prestigeUpgradeListHTML += `
+      <div class="prestige-upgrade">
+        <h3>${prestigeUpgrade.name}</h3>
+        <p>Cost: ${numberWithCommas(prestigeUpgradeCost(prestigeUpgrade).toFixed(0))} Time Essence</p>
+        <p>Bonus Multiplier: ${(prestigeUpgrade.bonusMultiplier * 100).toFixed(1)}%</p>
+        <p>Owned: ${prestigeUpgrade.owned}</p>
+        <button onclick="purchasePrestigeUpgrade('${prestigeUpgrade.id}')"${buttonDisabled}>Buy</button>
+      </div>
+    `;
+    }
+
+    if (document.getElementById("prestige-upgrades").innerHTML !== prestigeUpgradeListHTML) {
+        document.getElementById("prestige-upgrades").innerHTML = prestigeUpgradeListHTML;
+    }
 }
 
 function upgradeCost(upgrade) {
@@ -457,13 +526,29 @@ function updateAchievementDisplay() {
     document.getElementById('achievement-list').innerHTML = achievementListHTML;
 }
 
+function mergeUpgrades(originalUpgrades, gameDataUpgrades) {
+    const mergedUpgrades = originalUpgrades.map(upgrade => {
+        const matchingUpgrade = gameDataUpgrades.find(gdUpgrade => gdUpgrade.id === upgrade.id);
+
+        if (matchingUpgrade) {
+            return { ...upgrade, ...matchingUpgrade };
+        }
+
+        return upgrade;
+    });
+
+    return mergedUpgrades;
+}
+
 function saveGame() {
     const gameData = {
         timeCrystals,
+        timeEssence,
         crystalsPerSecond,
         lastUpdate,
         upgrades,
-        achievements
+        achievements,
+        prestigeUpgrades: prestigeUpgrades
     };
     localStorage.setItem('timeTravelAgency', JSON.stringify(gameData));
 }
@@ -473,9 +558,11 @@ function loadGame() {
     if (savedData) {
         const gameData = JSON.parse(savedData);
         timeCrystals = gameData.timeCrystals;
+        timeEssence = gameData.timeEssence || 0;
         crystalsPerSecond = gameData.crystalsPerSecond;
         lastUpdate = gameData.lastUpdate;
-        upgrades = gameData.upgrades;
+        upgrades = mergeUpgrades(upgrades, gameData.upgrades);
+        prestigeUpgrades = mergeUpgrades(prestigeUpgrades, gameData.prestigeUpgrades || []);
         if (gameData.achievements) achievements = gameData.achievements;
     }
 }
@@ -486,6 +573,61 @@ function numberWithCommas(x, decimals) {
     return parts.join(".");
 }
 
+function resetForPrestige() {
+    const essenceReward = timeEssenceReward();
+    if (confirm(`Are you sure you want to reset your progress for ${essenceReward} Time Essence? This action cannot be undone.`)) {
+        timeEssence += essenceReward;
+        // Reset game state
+        timeCrystals = 0;
+        crystalsPerSecond = 0;
+        upgrades = upgrades.map(upgrade => {
+            upgrade.owned = 0;
+            return upgrade;
+        });
+        achievements = achievements.map(achievement => {
+            achievement.unlocked = false;
+            return achievement;
+        });
+    }
+}
+
+function timeEssenceReward() {
+    return Math.floor(Math.sqrt(timeCrystals / 1e6));
+}
+
+function purchasePrestigeUpgrade(prestigeUpgradeId) {
+    const prestigeUpgrade = prestigeUpgrades.find((pu) => pu.id === prestigeUpgradeId);
+    if (prestigeUpgrade && canBuyPrestigeUpgrade(prestigeUpgrade)) {
+        timeEssence -= prestigeUpgradeCost(prestigeUpgrade);
+        prestigeUpgrade.owned += 1;
+
+        if (prestigeUpgrade.id === "timeTravelerEfficiency") {
+            for (const upgrade of upgrades) {
+                if (upgrade.id.startsWith("timeTraveler")) {
+                    upgrade.cps *= (1 + prestigeUpgrade.bonusMultiplier);
+                }
+            }
+        }
+
+        if (prestigeUpgrade.id === "timeMachineUpgradeDiscount") {
+            for (const upgrade of upgrades) {
+                if (upgrade.id.startsWith("timeMachine")) {
+                    upgrade.costMultiplier *= (1 - prestigeUpgrade.bonusMultiplier);
+                }
+            }
+        }
+
+        updateDisplay();
+    }
+}
+
+function canBuyPrestigeUpgrade(prestigeUpgrade) {
+    return timeEssence >= prestigeUpgradeCost(prestigeUpgrade);
+}
+
+function prestigeUpgradeCost(prestigeUpgrade) {
+    return Math.floor(prestigeUpgrade.baseCost * Math.pow(prestigeUpgrade.costMultiplier, prestigeUpgrade.owned));
+}
 
 /*Game loop*/
 
@@ -511,3 +653,11 @@ setInterval(saveAndCheckAchievements, 5000);
 /*Add event listeners*/
 
 document.getElementById('gather-crystals').addEventListener('click', gatherCrystals);
+document.getElementById('reset-game').addEventListener('click', resetGame);
+
+function resetGame() {
+    if (confirm('Are you sure you want to reset the game? This action cannot be undone.')) {
+        localStorage.removeItem('timeTravelAgency');
+        location.reload();
+    }
+}
