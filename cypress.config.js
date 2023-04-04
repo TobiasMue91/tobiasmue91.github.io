f7971b5dc2e60e142975cf0f836294dcae1df2d6
TobiasMue91 <tobias.mue91@googlemail.com>
2023-04-04 15:58:50 +0200

    - change port to 8080

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:8080/',
   },
 });
