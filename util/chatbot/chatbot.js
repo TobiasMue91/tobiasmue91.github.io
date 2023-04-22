8b7754bc26e9db897a7aeb92941adee4409a1f45
TobiasMue91 <tobias.mue91@googlemail.com>
2023-04-23 00:14:46 +0200

    - use similarity comparison to improve the data that is returned by the `fetchSearchResults` method - add better error handling - save/load conversation in/from local history - add "Thinking..." indicator - add chat toggle icon - add general information about the page - add chatbot to index

--- util/chatbot/chatbot.js
+++ util/chatbot/chatbot.js
@@ -35,20 +35,24 @@ class RetrievalAssistant {
             return message.message();
         } catch (error) {
             console.error('Request failed:', error);
+            return { role: 'assistant', content: 'I am sorry, but I am unable to process your request at the moment. Please try again later.' };
         }
     }
 
-    async fetchSearchResults(prompt) {
-        // Remove special characters and split the prompt into words
-        const keywords = prompt.toLowerCase().replace(/[^\w\s]/g, '').split(' ').filter(word => word.length > 3);
 
+    async fetchSearchResults(prompt) {
+        const threshold = 0.8; // Adjust the similarity threshold as needed
+        const keywords = prompt.toLowerCase().replace(/[^\w\s]/g, '').split(' ').filter(word => word.length > 3);
         const results = [];
 
         for (const key in jsonData) {
             const entryKeywords = key.split(',').flatMap(keyword => keyword.split(' '));
-            if (keywords.some(keyword => entryKeywords.some(entryKeyword => entryKeyword.includes(keyword) || keyword.includes(entryKeyword)))) {
+            if (keywords.some(keyword => entryKeywords.some(entryKeyword => {
+                const similarity = jaroWinkler(keyword, entryKeyword);
+                return similarity >= threshold;
+            }))) {
                 results.push(jsonData[key]);
-                if (results.length >= 10) {
+                if (results.length >= 5) {
                     break;
                 }
             }
@@ -58,20 +62,28 @@ class RetrievalAssistant {
     }
 
     async askAssistant(userMessage) {
+        if (!userMessage.content.trim()) return;
+
         this.conversationHistory.push(userMessage);
         const searchResults = await this.fetchSearchResults(userMessage.content);
 
         if (searchResults) {
             let resultsText = '';
-            for (const [index, result] of searchResults.entries()) {
-                resultsText += `Result ${index + 1}: ${result}\n`;
+            if (typeof searchResults[0] === 'string') {
+                for (const [index, result] of searchResults.entries()) {
+                    resultsText += `Result ${index + 1}: ${result}\n`;
+                }
+            } else {
+                resultsText = `Keywords: ${searchResults.join(', ')}\n`;
             }
-            this.conversationHistory.push({ "role": "system", "content": `Answer the user's question using the following data:\n${resultsText}` });
-            const assistantResponseWithContext = await this.fetchAssistantResponse(this.conversationHistory);
+
+            this.conversationHistory.push({ "role": "system", "content": `Answer the user's question using the following data, if the data seems relevant to the question:\n${resultsText}` });
+            const assistantResponseWithContext = await this.fetchAssistantResponse(this.conversationHistory.slice(-4));
             this.conversationHistory.push(assistantResponseWithContext);
             return assistantResponseWithContext;
         } else {
-            const assistantResponse = await this.fetchAssistantResponse(this.conversationHistory);
+            this.conversationHistory.push({ "role": "system", "content": `The user has asked about something that we don't have any data about. Try to say something nice and completely irrelevant to distract the user from this failure.` })
+            const assistantResponse = await this.fetchAssistantResponse(this.conversationHistory.slice(-4));
             this.conversationHistory.push(assistantResponse);
             return assistantResponse;
         }
@@ -79,20 +91,19 @@ class RetrievalAssistant {
 }
 
 (async () => {
-    const response = await fetch('chatbot.html');
+    var scriptFolderPath = document.currentScript.src.substring(0, document.currentScript.src.lastIndexOf('/') + 1);
+    const response = await fetch(scriptFolderPath + 'chatbot.html');
     const chatbotHtml = await response.text();
     const chatbotWrapper = document.createElement('div');
     chatbotWrapper.innerHTML = chatbotHtml;
     document.body.appendChild(chatbotWrapper);
-
     const fetchJSONData = async function() {
-        const response = await fetch('chatbot.json');
+        const response = await fetch(scriptFolderPath + 'chatbot.json');
         return await response.json();
     }
 
     jsonData = await fetchJSONData();
 
-    // Add the script from chatbot.html here
     const sendButton = document.getElementById('sendButton');
     const userInput = document.getElementById('userInput');
     const messagesDiv = document.getElementById('messages');
@@ -107,6 +118,20 @@ class RetrievalAssistant {
         messagesDiv.scrollTop = messagesDiv.scrollHeight;
     }
 
+    function showThinkingMessage() {
+        const thinkingMessage = {
+            role: 'assistant',
+            content: 'Thinking...'
+        };
+        addMessageToUI(thinkingMessage.role, thinkingMessage.content);
+    }
+
+    function removeMessageFromUI() {
+        const messages = messagesDiv.children;
+        if (messages.length > 0) {
+            messagesDiv.removeChild(messages[messages.length - 1]);
+        }
+    }
     function showWelcomeMessage() {
         const welcomeMessage = {
             role: 'assistant',
@@ -120,7 +145,11 @@ class RetrievalAssistant {
         addMessageToUI(userMessage.role, userMessage.content);
         userInput.value = '';
 
+        // Show the "Thinking..." message
+        showThinkingMessage();
         const assistantResponse = await assistant.askAssistant(userMessage);
+        localStorage.setItem('conversationHistory', JSON.stringify(assistant.conversationHistory));
+        removeMessageFromUI();
         addMessageToUI(assistantResponse.role, assistantResponse.content);
     });
 
@@ -130,6 +159,78 @@ class RetrievalAssistant {
         }
     });
 
-    // Call the showWelcomeMessage function
+    function loadPreviousMessages() {
+        for (const message of assistant.conversationHistory) {
+            addMessageToUI(message.role, message.content);
+        }
+    }
+
+    const toggleChat = document.getElementById('toggleChat');
+    const chatbotHeader = document.getElementById('chatbotHeader');
+    const chatbotWindow = document.getElementById('chatbot');
+
+    [toggleChat, chatbotHeader].forEach(element => element.addEventListener('click', () => {
+        if (chatbotWindow.style.display === 'none') {
+            chatbotWindow.style.display = 'flex';
+            toggleChat.style.display = 'none';
+        } else {
+            chatbotWindow.style.display = 'none';
+            toggleChat.style.display = 'flex';
+        }
+    }));
+
     showWelcomeMessage();
+    loadPreviousMessages();
 })();
+
+function jaroWinkler(s1, s2) {
+    let m = 0;
+    if (s1.length === 0 || s2.length === 0) return 0;
+    if (s1 === s2) return 1;
+
+    let range = (Math.floor(Math.max(s1.length, s2.length) / 2)) - 1;
+    let s1Matches = new Array(s1.length);
+    let s2Matches = new Array(s2.length);
+
+    for (let i = 0; i < s1.length; i++) {
+        let low = (i >= range) ? i - range : 0;
+        let high = (i + range <= (s2.length - 1)) ? (i + range) : (s2.length - 1);
+
+        for (let j = low; j <= high; j++) {
+            if (s1Matches[i] !== true && s2Matches[j] !== true && s1[i] === s2[j]) {
+                ++m;
+                s1Matches[i] = s2Matches[j] = true;
+                break;
+            }
+        }
+    }
+
+    if (m === 0) return 0;
+
+    let k = 0;
+    let numTrans = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1Matches[i] === true) {
+            let j;
+            for (j = k; j < s2.length; j++) {
+                if (s2Matches[j] === true) {
+                    k = j + 1;
+                    break;
+                }
+            }
+            if (s1[i] !== s2[j]) ++numTrans;
+        }
+    }
+
+    let weight = (m / s1.length + m / s2.length + (m - (numTrans / 2)) / m) / 3;
+    let l = 0;
+    let p = 0.1;
+
+    if (weight > 0.7) {
+        while (s1[l] === s2[l] && l < 4) ++l;
+        weight = weight + l * p * (1 - weight);
+    }
+
+    return weight;
+}
