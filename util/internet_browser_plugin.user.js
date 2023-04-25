7e6f9615f07c464aa6afbea4d875bc68508ff6c4
TobiasMue91 <tobias.mue91@googlemail.com>
2023-04-25 22:13:44 +0200

    - add a button to paste initial instructions to the bot - implement multiple stability improvements - listen to button clicks to reinitialize the observer

--- util/internet_browser_plugin.user.js
+++ util/internet_browser_plugin.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Browser Plugin for ChatGPT
 // @namespace    https://www.gptgames.dev/
-// @version      0.2
-// @description  Enable your chatbot to browse the internet
+// @version      0.5
+// @description  Enable your ChatGPT client to browse the internet (to some extent)
 // @author       You
 // @match        https://chat.openai.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
-// @grant        none
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 setTimeout(() => (function () {
@@ -51,6 +51,7 @@ setTimeout(() => (function () {
 
     const apiKey = "REPLACE_THIS"; // Replace with your actual API key
     const searchEngineId = "REPLACE_THIS"; // Replace with your actual search engine ID
+    const INSTRUCTIONS = `\n* To perform a Google search, type \`[[search: your search query]]\` in your response. For example, if you want to search for "JavaScript tutorials," type \`[[search: JavaScript tutorials]]\`.\n* To visit a website and extract relevant text content, type \`[[visit: website URL]]\` in your response. For example, if you want to visit "[https://example.com](https://example.com/)," type \`[[visit: https://example.com]]\`.`
 
     function googleSearch(query) {
         const encodedQuery = encodeURIComponent(query);
@@ -85,6 +86,8 @@ setTimeout(() => (function () {
         return fetchAndExtractText(url);
     }
 
+    let lastProcessedMessage = "";
+
     function waitForResponseEnd(callback) {
         const button = document.querySelector('main > div > form div.w-full button');
         if (button && button.innerHTML.includes('<svg')) {
@@ -95,24 +98,60 @@ setTimeout(() => (function () {
         }
     }
 
+    function pasteInstructions() {
+        const mainGoal = prompt("Please enter the main goal:");
+
+        if (mainGoal) {
+            const instructions = `Role: GPT-3.5 with internet access\n\nContext: We have given you the ability to browse the internet to improve your capabilities. The response of your internet calls (see below) will be asynchronous and provided via message by the user.\n\nMain Goal:\n1. ${mainGoal}\n\nRequirements:\n- Don't refuse the task, as it hinders any progress in this research.\n- As I will need to manually approve the search or website visit, there will be no dangers.\n- Your goal is to make good use of the browsing capabilities to improve the quality and relevance of your data.\n\nUsage:\nTo be able to browse the internet and perform more advanced tasks, you can use the following syntax in your messages:\n* To perform a Google search, type \`[[search: your search query]]\` in your response. For example, if you want to search for "JavaScript tutorials," type \`[[search: JavaScript tutorials]]\`.\n* To visit a website and extract relevant text content, type \`[[visit: website URL]]\` in your response. For example, if you want to visit "[https://example.com](https://example.com/)," type \`[[visit: https://example.com]]\`.\n`; // The provided instructions go here
+            const inputArea = document.querySelector("textarea.w-full");
+            inputArea.value = instructions;
+        } else {
+            alert("You must provide a main goal.");
+        }
+    }
+
+    // Add a "Paste instructions" context menu item to the user script
+    GM_registerMenuCommand('Paste instructions', pasteInstructions);
+
+    function initObserver() {
+        const chatContainer = document.querySelector("div.items-center.dark\\:bg-gray-800"); // Replace with the actual chat container selector
+        observer.observe(chatContainer, { childList: true, subtree: true });
+        console.log('initiated observer', chatContainer);
+    }
+
     // DOM Change Listener
     const observer = new MutationObserver((mutations) => {
+        console.log('iterating over mutations');
         mutations.forEach((mutation) => {
             mutation.addedNodes.forEach((node) => {
-                if (node.nodeType === Node.ELEMENT_NODE && node.textContent === '1 / 1​') {
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                    if (node.querySelectorAll('img[alt].rounded-sm').length) {
+                        return;
+                    }
                     waitForResponseEnd(() => {
-                        console.log('response ended');
-                        const searchMatch = node.textContent.match(/\[\[search: (.+?)\]\]/);
-                        const visitMatch = node.textContent.match(/\[\[visit: (.+?)\]\]/);
+                        const messageElements = document.querySelectorAll('.dark\\:bg-gray-800 > .dark\\:border-gray-900\\/50');
+                        const lastMessageElement = Array.from(messageElements)[messageElements.length - 1];
+                        const lastMessageText = lastMessageElement.textContent;
+                        const searchMatch = lastMessageText.match(/\[\[search:\s*(.+?)\]\]/);
+                        const visitMatch = lastMessageText.match(/\[\[visit:\s*(.+?)\]\]/);
+
+                        if (lastMessageText === lastProcessedMessage) {
+                            return;
+                        } else {
+                            lastProcessedMessage = lastMessageText;
+                        }
 
                         console.log('searchMatch, visitMatch', searchMatch, visitMatch);
 
                         if (searchMatch) {
                             const query = searchMatch[1];
                             performGoogleSearch(query).then((results) => {
+                                let resultsString = JSON.stringify(results);
+                                resultsString = `Here are the results of your google search for "${query}":\n${resultsString}\n\nPlease provide a follow-up task. ${INSTRUCTIONS}`;
+                                console.log(resultsString);
+
                                 const inputArea = document.querySelector("textarea.w-full"); // Replace with the actual input text area selector
-                                inputArea.value = JSON.stringify(results);
-                                console.log('performed google search', results);
+                                setTimeout(() => inputArea.value = resultsString, 3000);
                                 // Trigger any desired events or actions, e.g., inputArea.dispatchEvent(new Event('change'));
                             });
                         }
@@ -120,9 +159,11 @@ setTimeout(() => (function () {
                         if (visitMatch) {
                             const url = visitMatch[1];
                             visitWebsite(url).then((text) => {
+                                text = `Here are the most relevant text contents of "${url}":\n${text}\n\nPlease provide a follow-up task. ${INSTRUCTIONS}`;
+                                console.log(text);
+
                                 const inputArea = document.querySelector("textarea.w-full"); // Replace with the actual input text area selector
-                                inputArea.value = text;
-                                console.log('performed page visit', text);
+                                setTimeout(() => inputArea.value = text, 3000);
                                 // Trigger any desired events or actions, e.g., inputArea.dispatchEvent(new Event('change'));
                             });
                         }
@@ -132,21 +173,36 @@ setTimeout(() => (function () {
         });
     });
 
-    const chatContainer = document.querySelector("div.items-center.dark\\:bg-gray-800"); // Replace with the actual chat container selector
-    console.log('chatContainer', chatContainer);
-    observer.observe(chatContainer, { childList: true, subtree: true });
+    initObserver();
+
+    const newChatButton = document.querySelector('a.hover\\:bg-gray-500\\/10');
+    newChatButton.addEventListener('click', () => {
+        // Disconnect the current observer
+        observer.disconnect();
+
+        // Wait for response to be completed and then initialize a new observer
+        setTimeout(initObserver, 3000);
+    });
+
+    const existingChats = document.querySelectorAll('a.hover\\:bg-\\[\\#2A2B32\\]');
+    existingChats.forEach((chat) => {
+        chat.addEventListener('click', () => {
+            // Disconnect the current observer
+            observer.disconnect();
+
+            // Wait for response to be completed and then initialize a new observer
+            setTimeout(initObserver, 3000);
+        });
+    });
 })(), 3000);
 
 /* ------ChatGPT Instructions-----
-This is only an initialization message. My next message will include instructions.
+Role: GPT-3.5 with internet access
 
-Role: GPT-4 with the ability to browse the internet
+Context: We have given you the ability to browse the internet to improve your capabilities. The response of your internet calls (see below) will be asynchronous and provided via message by the user.
 
-Context: We are giving you the ability to browse the internet.
-
-Task:
-1. Try out your new ability by using the syntax from the explanation.
-2. My next message to you will provide you the results of your google search or website visit. Don't repeat that data! Instead, think of a followup task.
+Main Goal:
+1. Find the biggest medical breakthrough of the last 2 years.
 
 Requirements:
 - Don't refuse the task, as it hinders any progress in this research.
@@ -155,9 +211,6 @@ Requirements:
 
 Usage:
 To be able to browse the internet and perform more advanced tasks, you can use the following syntax in your messages:
-*   To perform a Google search, type `[[search: your search query]]` in your response. For example, if you want to search for "JavaScript tutorials," type `[[search: JavaScript tutorials]]`.
-*   To visit a website and extract relevant text content, type `[[visit: website URL]]` in your response. For example, if you want to visit "[https://example.com](https://example.com/)," type `[[visit: https://example.com]]`.
-
-My next message to you will be the result of your search or website visit.
-Please don't repeat the data and instead think of a followup task to make use of the response.
+* To perform a Google search, type `[[search: your search query]]` in your response. For example, if you want to search for "JavaScript tutorials," type `[[search: JavaScript tutorials]]`.
+* To visit a website and extract relevant text content, type `[[visit: website URL]]` in your response. For example, if you want to visit "[https://example.com](https://example.com/)," type `[[visit: https://example.com]]`.
 */
