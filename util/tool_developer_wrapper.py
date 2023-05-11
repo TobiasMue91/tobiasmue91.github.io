a6d845a052930b1ca2cd5e14cdfc6cb579d3bd97
TobiasMue91 <tobias.mue91@googlemail.com>
2023-05-11 21:13:25 +0200

    - add file type identifier

--- util/tool_developer_wrapper.py
+++ util/tool_developer_wrapper.py
@@ -22,12 +22,12 @@ def generate_new_tool_names(existing_tools, num_options=8):
     load_dotenv()
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
-    forbidden_tools = ["URL Shortener", "Image Watermarker", "Currency Converter", "File Type Converter", "IP Address Locator", "Text to Speech Converter", "Password Manager", "File Type Detector", "Audio Speed Changer", "Image to ASCII Art Converter", "Markdown Editor", "Online Mind Map Maker", "Image to PDF Converter"]
+    forbidden_tools = ["Image Color Palette Extractor", "URL Shortener", "Image Watermarker", "Currency Converter", "File Type Converter", "IP Address Locator", "Text to Speech Converter", "Password Manager", "File Type Detector", "Audio Speed Changer", "Image to ASCII Art Converter", "Markdown Editor", "Online Mind Map Maker", "Image to PDF Converter"]
 
     messages = [
         {"role": "assistant", "content": "As an AI expert in tool development, I can generate ideas for standalone web tools that work without backend functionality.The tools should be entirely client-side, not relying on any server-side processing. My output will have the format: \"\nUnit Converter: Transform units like a boss! This unit converter is so epic, it'll make your head spin.\nJSON Formatter: Transform messy JSON into a beautiful and organized format with just a click! Copy and paste your JSON code and voila!\n...\n\".\nMy suggestions will be creative and good fitting additions to the existing tools."},
         {"role": "system", "content": f"Suggestion blacklist: {', '.join(forbidden_tools)}\nExisting tools: {', '.join(existing_tools)}."},
-        {"role": "user", "content": f"Generate {num_options} tool names for practical standalone web tools that work entirely on the client side without relying on any backend or server-side functionality. Don't suggest tools that already exist."}
+        {"role": "user", "content": f"Generate {num_options} tool names for practical standalone web tools that work entirely on the client side without relying on any backend or server-side functionality. Don't suggest tools that already exist. Don't suggest tools that are very similar to tools that already exist."}
     ]
 
     try:
