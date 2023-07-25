9c37d63e2c13ffd06cb59b0ba52f5a46b2063695
TobiasMue91 <tobias.mue91@googlemail.com>
2023-07-25 14:58:34 +0200

    - add plugins_split script to prepare for rudimentary pagination

---
+++ util/plugins_split.py
@@ -0,0 +1,26 @@
+import json
+import os
+from pathlib import Path
+
+# Get the absolute path to the Python script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Open the JSON file with utf-8 encoding and load its data
+with open(os.path.join(script_dir, "plugins.json"), "r", encoding='utf-8') as f:
+    data = json.load(f)
+
+# Define the chunk size
+chunk_size = 100
+
+# Split the items into chunks
+chunks = [data["items"][i:i + chunk_size] for i in range(0, len(data["items"]), chunk_size)]
+
+# Create the plugins directory if it doesn't exist
+plugins_dir = os.path.join(script_dir, "plugins")
+Path(plugins_dir).mkdir(parents=True, exist_ok=True)
+
+# Write each chunk to a separate JSON file
+for i, chunk in enumerate(chunks, 1):
+    chunk_data = {"items": chunk, "count": len(chunk)}
+    with open(os.path.join(plugins_dir, f"plugins_{i}.json"), "w", encoding='utf-8') as f:
+        json.dump(chunk_data, f, indent=4)
